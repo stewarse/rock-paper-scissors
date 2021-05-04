813ed12db2bcd553718d3352026b6fd57210490f
Sean Stewart <stewarse@gmail.com>
2021-05-03 19:47:03 -0700

    Updated start logic to only run if user and comp scores are below threshold

--- main.js
+++ main.js
@@ -3,10 +3,11 @@
  * determine the computers choice
  * 
  * Create a start button that allows a user to start a new game.
- *  Should reset the game back to it's original state, all scores and counters back to init values 
+ *  Should reset the game back to it's original state, all scores and counters back to init values - -DONE (needs testing)
  * play a round of the game with players choice and computer choice - DONE
- *  Record the winner of the round via a score
- *  End the game when one player reaches 5
+ *   Record the winner of the round and increment that players score
+ *   Update the score in the DOM 
+ *   End the game when one players score reaches 5
  * select the winner based on a set of rules - DONE
  **/
 const choiceArr = [ 'rock', 'paper', 'scissors' ]
@@ -34,47 +35,52 @@ let compScore = 0;
 
 
 
-function playRound( e, comp) {
+function playRound( e ) {
     
 //     if ( e.target.nodeName === "I") {
 //         console.log(e.target.textContent);
 //     }
-    user = e.target.value
+    let userChoice = e.target.value
     //user = user.toLowerCase()
-    comp = computerPlay()
+    // compChhoice = computerPlay()
 
-    let winLoseTie = winner ='are victorious',
-    winningChoice = user,
-    losingChoice = comp,
-    verb = 'beats';
+    let winLoseTie = 'are victorious'
+    , winningChoice = userChoice
+    , losingChoice = compChoice
+    , verb = 'beats'
+    , roundWinner = 'user';
 
-    if (user === comp){
+    if (userChoice === compChoice){
         winLoseTie = 'tied'
         verb = 'ties'
-    } else if (user === 'rock'){
-        if (comp === "paper") {
+    } else if (userChoice === 'rock'){
+        if (compChoice === "paper") {
             winLoseTie = 'lose';
+            winner = 'comp'
         }
-    } else if (user === 'paper'){
-        if (comp === "scissors") {
+    } else if (userChoice === 'paper'){
+        if (compChoice === "scissors") {
             winLoseTie = 'lose';
+            winner = 'comp'
         }
     } else {
-        if (comp === "rock") {
+        if (compChoice === "rock") {
             winLoseTie = 'lose';
+            winner = 'comp'
         }
     }
 
     if(winLoseTie === 'lose'  ) {
-        winningChoice = comp;
-        losingChoice = user;
+        winningChoice = compChoice;
+        losingChoice = userChoice;
     }
 
     result.textContent = `You ${winLoseTie}! ${winningChoice} ${verb} ${losingChoice}`;
 
     console.log(`You ${winLoseTie}! ${winningChoice} ${verb} ${losingChoice}`);
-
-    return winningChoice;
+    if ( winLoseTie !== 'tied') {
+        updateScore(roundWinner);
+    }
 }
 
 //Determine the computers choice by selecting from the choices 
@@ -84,31 +90,47 @@ function computerPlay() {
 
 //Write a game function that plays a 5 round game 
 function game() {
-
-        compChoice = computerPlay();
-        buttons.forEach((button) => {
-            button.addEventListener('click', playRound);
-        });
+    compChoice = computerPlay();
+    buttons.forEach((button) => {
+        button.addEventListener('click', playRound);
+    });
+    console.log(roundWinner);
 }
 
 start.addEventListener('click',startGame);
 
 function startGame() {
-    if (activeGame === true) {
-        alert('Please choose your weapon')      
-    } else if(alreadyPlayed === true) {
-        activeGame = true;
-        //reset the scores
-        resetScore()
-        //initialize the game
-        game();
+    if(userScore !== 5 && compScore != 5) {
+        if (activeGame === true) {
+            alert('Please choose your weapon')      
+        } else if(alreadyPlayed === true) {
+            activeGame = true;
+            //reset the scores
+            resetScore()
+            //initialize the game
+            game();
+        } else {
+            activeGame = true;
+            game();  
+        }
     } else {
-        activeGame = true;
-        game();  
+        declareWinner()
     }
 }
 
 function resetScore () {
     userScore = 0;
     compScore = 0;
+}
+
+function updateScore(winner){
+    if(winner === 'user') {
+        userScore += 1
+    } else {
+        compScore += 1
+    }
+}
+
+function declareWinner(){
+
 }
